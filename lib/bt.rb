7d088beb1436fedc33f1cfb1586edaceeb352c09
Scott Robinson <scott.robinson@thoughtworks.com>
2011-04-03 14:29:07 +1000

    bt/ready now uses new class-based APIs.

--- lib/bt.rb
+++ lib/bt.rb
@@ -1,28 +1,57 @@
 module BT
+  require 'andand'
   require 'yaml'
 
+  class Stage < Hash
+    # TODO: Comparison operators. (Taking into account commit + filename?)
+    # TODO: A more informative to_s.
+    
+    def initialize(commit, filename)
+      @commit = commit
+      @filename = filename
+      merge!(YAML::load `git cat-file blob #{commit}:#{filename}`)
+    end
+
+    def name
+      File.basename(@filename)
+    end
+
+    def needs
+      (self['needs'] || []).map do |stage_name|
+        Stage.new @commit, File.join(File.dirname(@filename), stage_name)
+      end
+    end
+
+    [:run, :results].each { |n| define_method(n) { self[n.to_s] } }
+  end
+
   class Repository
+    def initialize(path)
+      @path = path
+      # TODO: Do something with the path.
+      @head = commit 'HEAD'
+    end
+
     def ready
       dones = done
 
       [].tap do |readies|
-        stages.each do |name, info|
-          needs = info['needs'] ? info['needs'] - dones : []
-          readies << name if needs.empty?
+        (stages - dones).each do |stage|
+          needs = stage.needs - dones
+          readies << stage if needs.empty?
         end
       end - dones
     end
 
     private
-    def stages
-      # TODO: Enforce stages being politely named.
-      Hash[`git ls-tree --name-only HEAD stages/`.split.map do |fn|
-        [File.basename(fn), YAML.load(`git cat-file blob HEAD:#{fn}`)]
-      end]
-    end
-
     def commit(name)
       `git rev-parse --verify #{name}`.strip
+    end
+
+    def stages
+      `git ls-tree --name-only #{@head} stages/`.split.map do |fn|
+        Stage.new(@head, fn)
+      end
     end
 
     def done
@@ -31,7 +60,7 @@ module BT
           %r{ \[bt/(?<hash>[0-9a-f]+)/(?<stage>\w+)\] (?<status>OK|PASS|FAIL|NO) } =~ branch_line
           oks << stage if ['OK', 'PASS'].include? status
         end
-      end
+      end.map { |s| Stage.new @head, s }
     end
   end
 end
