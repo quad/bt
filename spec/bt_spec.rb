c05c3b985586e503875e77e59f75569402247b9a
Andrew Kiellor <akiellor@gmail.com>
2011-06-07 17:37:31 +1000

    Added specs for multistage builds
      * Made a small internal DSL for setting up test repos with BT Builds

--- spec/bt_spec.rb
+++ spec/bt_spec.rb
@@ -3,16 +3,17 @@ require 'grit'
 
 describe "a repo with a bt build" do
   before do
-    FileUtils.cd(Dir.mktmpdir) do |dir|
-      @repo = Grit::Repo.init(dir)
-      FileUtils.makedirs("#{@repo.working_dir}/stages")
-      File.open("#{@repo.working_dir}/stages/first", 'w') {|f| f.write("run: echo \"blah\" > new_file\nresults:\n  - new_file") }
-      @repo.add("stages/first")
-      @repo.commit_all("Initial commit")
-      @initial_commit = @repo.commits.first
+    @repo = BT::Builder.repo_at(Dir.mktmpdir) do |r|
+      r.stage 'first', <<-eos
+run: echo \"blah\" > new_file
+results:
+  - new_file
+      eos
     end
 
-    %x[./bin/bt go #{@repo.working_dir} 2> /dev/null]
+    %x{./bin/bt go #{@repo.working_dir} 2> /dev/null}
+
+    @initial_commit = @repo.commits.first
   end
 
   subject { @repo }
@@ -23,6 +24,54 @@ describe "a repo with a bt build" do
     subject { @repo.tree("bt/#{@initial_commit.sha}/first") }
 
     it { should have_file_content('new_file', "blah\n") }
+  end
+end
+
+describe "a repo with two dependent stages" do
+  before do
+    @repo = BT::Builder.repo_at(Dir.mktmpdir) do |r|
+      r.stage 'first', <<-eos
+run: echo \"blah\" > new_file
+results:
+  - new_file
+      eos
+
+      r.stage 'second', <<-eos
+run: echo \"blah blah\" >> new_file
+needs:
+  - first
+results:
+  - new_file
+      eos
+    end
+  end
+
+  subject { @repo }
+
+  let(:first_commit) { @repo.commits.first }
+  
+  context "first stage built" do
+    before { %x{./bin/bt go #{@repo.working_dir} 2> /dev/null}; }
+    
+    it { should have_head "bt/#{first_commit.sha}/first" }
+
+    context "its results tree" do
+      subject { @repo.tree("bt/#{first_commit.sha}/first") }
+
+      it { should have_file_content('new_file', "blah\n") }
+    end
+ end
+
+  context "second stage built" do
+    before { 2.times { %x{./bin/bt go #{@repo.working_dir} 2> /dev/null} } }
+
+    it { should have_head "bt/#{first_commit.sha}/second" }
+
+    context "its results tree" do
+      subject { @repo.tree("bt/#{first_commit.sha}/second") }
+
+      it { should have_file_content('new_file', "blah\nblah blah\n") }
+    end
   end
 end
 
@@ -37,3 +86,30 @@ RSpec::Matchers.define :have_file_content do |name, content|
     (tree / name).data == content
   end
 end
+
+module BT
+  class Builder
+    def self.repo_at dir, &block
+      FileUtils.cd(dir) do |dir|
+        return new(dir, &block).repo
+      end
+    end
+
+    attr_reader :repo
+
+    def initialize dir, &block
+      @repo = Grit::Repo.init(dir)
+      yield self
+      @repo.commit_all("Initial commit")
+    end
+
+    def stage name, stage_config
+      FileUtils.makedirs("#{@repo.working_dir}/stages")
+      File.open("#{@repo.working_dir}/stages/#{name}", 'w') do |f|
+        f.write(stage_config)
+      end
+      @repo.add "stages/#{name}"
+    end
+  end
+end
+
