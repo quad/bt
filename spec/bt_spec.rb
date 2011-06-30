e85a0b18842c2885861b770bd748edafd2f38085
Andrew Kiellor <akiellor@gmail.com>
2011-06-30 15:13:51 +1000

    Moved Project and Macros out of the Integration spec

--- spec/bt_spec.rb
+++ spec/bt_spec.rb
@@ -1,15 +1,9 @@
-require 'forwardable'
-require 'grit'
-require 'yaml'
+require 'support/project'
 
 ENV['PATH'] = File.join(File.dirname(__FILE__), '/../bin') + ':' + ENV['PATH']
 
 describe 'bt-go' do
-  def self.project &block
-    let!(:project) { Project.at(Dir.mktmpdir, &block) }
-
-    subject { project }
-  end
+  include Project::RSpec
 
   describe "a repo with a bt build" do
     project do |p|
@@ -222,107 +216,3 @@ eos
   end
 end
 
-RSpec::Matchers.define :have_bt_ref do |stage, commit|
-  match do |project|
-    project.bt_ref(stage, commit)
-  end
-end
-
-RSpec::Matchers.define :have_file_content do |name, content|
-  match do |tree|
-    (tree / name).data == content
-  end
-
-  failure_message_for_should do |tree|
-    "Expected #{name.inspect} to have content #{content.inspect} but had #{(tree / name).data.inspect}"
-  end
-end
-
-RSpec::Matchers.define :have_results do |results|
-  match do |project|
-    result_string = project.results
-    results.all? do |stage, result_commit|
-      result_string.index /^#{stage.to_s}: (PASS|FAIL) bt loves you \(#{result_commit.sha}\)$/
-    end
-  end
-end
-
-class Project
-  class Ref < Grit::Ref
-    extend Forwardable
-
-    def_delegator :commit, :tree
-
-    def self.prefix
-      "refs/bt"
-    end
-  end
-
-  DEFAULT_STAGE_DEFINITION = {'run' => 'exit 0', 'needs' => [], 'results' => []}
-
-  def self.at dir, &block
-    FileUtils.cd(dir) do |dir|
-      return new(dir, &block)
-    end
-  end
-
-  attr_reader :repo
-
-  def initialize dir, &block
-    @repo = Grit::Repo.init(dir)
-    yield self
-    @repo.commit_all("Initial commit")
-  end
-
-  def stage name, stage_config
-    FileUtils.makedirs("#{@repo.working_dir}/stages")
-    File.open("#{@repo.working_dir}/stages/#{name.to_s}", 'w') do |f|
-      f.write(stage_config)
-    end
-    @repo.add "stages/#{name.to_s}"
-  end
-
-  def failing_stage name, overrides = {}
-    stage name, YAML.dump(DEFAULT_STAGE_DEFINITION.merge('run' => 'exit 1').merge(overrides))
-  end
-
-  def head
-    repo.commits.first
-  end
-
-  def passing_stage name, overrides = {}
-    stage name, YAML.dump(DEFAULT_STAGE_DEFINITION.merge(overrides))
-  end
-
-  def stage_generator name, generator_config
-    stage(name, generator_config)
-    File.chmod(0755, "#{@repo.working_dir}/stages/#{name.to_s}")
-  end
-
-  def bt_ref stage, commit
-    Ref.find_all(self.repo).detect { |r| r.name == "#{commit.sha}/#{stage}" }
-  end
-
-  def build
-    output = %x{bt-go --once --debug --directory #{repo.working_dir} 2>&1}
-    raise output unless $?.exitstatus.zero?
-  end
-
-  def results
-    output = %x{bt-results --debug --uri #{repo.working_dir} 2>&1}
-    raise output unless $?.exitstatus.zero?
-    output
-  end
-
-  def definition
-    output = %x{bt-stages #{repo.working_dir}}
-    raise output unless $?.exitstatus.zero?
-    output
-  end
-
-  def ready?
-    output = %x{bt-ready #{repo.working_dir}}
-    raise output unless $?.exitstatus.zero?
-    !output.empty?
-  end
-end
