460b74ccd27321c2afa72c975c085aa72c3f1e5a
Andrew Kiellor <akiellor@gmail.com>
2011-07-07 16:52:28 +1000

    Added some tests for bt-results

---
+++ spec/bt_results_spec.rb
@@ -0,0 +1,57 @@
+require 'support/project'
+
+ENV['PATH'] = File.join(File.dirname(__FILE__), '/../bin') + ':' + ENV['PATH']
+
+describe 'bt-results' do
+  include Project::RSpec
+
+  let(:first_result) { project.bt_ref('first', project.head).commit }
+  let(:second_result) { project.bt_ref('second', project.head).commit }
+
+  project do |p|
+    p.stage :first, <<-eos
+  run: echo \"blah\" > new_file
+  results:
+    - new_file
+    eos
+
+    p.stage :second, <<-eos
+  run: echo \"blah blah\" >> new_file
+  needs:
+    - first
+  results:
+    - new_file
+    eos
+  end
+
+  result_of_executing 'bt-results' do
+    should == <<-eos
+Results (#{project.head.sha}):
+
+first: 
+second: 
+    eos
+  end
+
+  executed 'bt-go --once' do
+    result_of_executing 'bt-results' do
+      should == <<-eos
+Results (#{project.head.sha}):
+
+first: PASS bt loves you (#{first_result.sha})
+second: 
+      eos
+    end
+  end
+
+  executed 'bt-go' do
+    result_of_executing 'bt-results' do
+      should == <<-eos
+Results (#{project.head.sha}):
+
+first: PASS bt loves you (#{first_result.sha})
+second: PASS bt loves you (#{second_result.sha})
+      eos
+    end
+  end
+end
